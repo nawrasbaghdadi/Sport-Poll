a72d95b5f0abf549e65b909011f8a704c90741d0
nawras <nawras@nawrass-MacBook-Pro.local>
2019-01-14 14:52:00 +0400

    chekcing git

--- src/compnents/ui/Poll.js
+++ src/compnents/ui/Poll.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import '../../stylesheets/poll.css';
-import { Services } from '../../service';
-import ShowMessage from './ShowMessage';
-
+import React from "react";
+import "../../stylesheets/poll.css";
+import { Services } from "../../service";
+import ShowMessage from "./ShowMessage";
 
 class Poll extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fetchingErr: false,
-      voted: (localStorage['voted']) ?
-        JSON.parse(localStorage.getItem('voted')) : [],
+      voted: localStorage["voted"]
+        ? JSON.parse(localStorage.getItem("voted"))
+        : [],
       data: null,
       current: null,
       randomArr: [],
-      message: '',
+      message: "",
       selected: 0
-
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleMouseOver = this.handleMouseOver.bind(this);
@@ -33,15 +32,18 @@ class Poll extends React.Component {
   }
   componentDidMount() {
     const feed = Services.getPoll();
-    feed.then((res) => {
+    feed.then(res => {
       if (res.length > 0) {
-        const random = this.randomVote(0, res.length - 1)
-        this.setState({ data: res, current: res[random], randomArr: [...this.state.randomArr, random] });
+        const random = this.randomVote(0, res.length - 1);
+        this.setState({
+          data: res,
+          current: res[random],
+          randomArr: [...this.state.randomArr, random]
+        });
+      } else {
+        this.setState({ fetchingErr: true, message: "Error In Fetching Data" });
       }
-      else {
-        this.setState({ fetchingErr: true, message: 'Error In Fetching Data' });
-      }
-    })
+    });
   }
 
   randomVote(min, max) {
@@ -52,8 +54,7 @@ class Poll extends React.Component {
       const x = this.state.randomArr.length - 1;
       if (max === x) {
         return -1;
-      }
-      else {
+      } else {
         return this.randomVote(min, max);
       }
     }
@@ -65,115 +66,123 @@ class Poll extends React.Component {
     if (document.querySelector('input[name="poll"]:checked') != null) {
       selected = document.querySelector('input[name="poll"]:checked').value;
     } else {
-      this.setState({ message: 'Please Select one option' });
+      this.setState({ message: "Please Select one option" });
       return;
     }
     const vote = {
       vote: this.state.current,
       selected: selected
-    }
+    };
 
     const randomVote = this.randomVote(0, this.state.data.length - 1);
     if (this.state.voted.length != this.state.data.length) {
       voted.push(vote);
-      localStorage.setItem('voted', JSON.stringify(voted));
+      localStorage.setItem("voted", JSON.stringify(voted));
       this.setState({ voted: voted });
       document.querySelector('input[name="poll"]:checked').checked = false;
       if (randomVote != -1) {
         this.setState({ current: this.state.data[randomVote] });
-        this.setState({ randomArr: [...this.state.randomArr, randomVote] })
+        this.setState({ randomArr: [...this.state.randomArr, randomVote] });
       } else {
-        this.setState({ message: 'Nice!! you finished the whole Votes ..' });
+        this.setState({ message: "Nice!! you finished the whole Votes .." });
       }
     } else {
-      this.setState({ message: 'You already finish all the votes for today , results saved in LocalStorege' });
+      this.setState({
+        message:
+          "You already finish all the votes for today , results saved in LocalStorege"
+      });
     }
-    const a = document.querySelector('.a');
-    this.handleMouseOut(a)
+    const a = document.querySelector(".a");
+    this.handleMouseOut(a);
   }
 
   handleChange(e) {
     this.setState({
       selected: e.target.value
-    })
+    });
   }
 
   handleMouseOver(target) {
-    const arr = Array.from(target.closest('.choices').querySelectorAll('div'));
-    const a = target.closest('.choices').querySelector('.a');
-    const b = target.closest('.choices').querySelector('.b');
-    const d = target.closest('.choices').querySelector('.d');
-    arr.map((item) => {
-      if (target.classList.contains('a') && item.classList.contains('a')) {
+    const arr = Array.from(target.closest(".choices").querySelectorAll("div"));
+    const a = target.closest(".choices").querySelector(".a");
+    const b = target.closest(".choices").querySelector(".b");
+    const d = target.closest(".choices").querySelector(".d");
+    arr.map(item => {
+      if (target.classList.contains("a") && item.classList.contains("a")) {
         while (item.classList.length > 0) {
           item.classList.remove(item.classList.item(0));
         }
-        item.classList.add('a');
-        item.classList.add('wins');
-        b.classList.add('loses');
-        d.classList.add('loses');
-      } else if (target.classList.contains('b') && item.classList.contains('b')) {
+        item.classList.add("a");
+        item.classList.add("wins");
+        b.classList.add("loses");
+        d.classList.add("loses");
+      } else if (
+        target.classList.contains("b") &&
+        item.classList.contains("b")
+      ) {
         while (item.classList.length > 0) {
           item.classList.remove(item.classList.item(0));
         }
-        item.classList.add('b');
-        item.classList.add('wins');
-        a.classList.add('loses');
-        d.classList.add('loses');
-      } else if (target.classList.contains('d') && item.classList.contains('d')) {
+        item.classList.add("b");
+        item.classList.add("wins");
+        a.classList.add("loses");
+        d.classList.add("loses");
+      } else if (
+        target.classList.contains("d") &&
+        item.classList.contains("d")
+      ) {
         while (item.classList.length > 0) {
           item.classList.remove(item.classList.item(0));
         }
-        item.classList.add('d');
-        item.classList.add('wins');
-        a.classList.add('loses');
-        b.classList.add('loses');
+        item.classList.add("d");
+        item.classList.add("wins");
+        a.classList.add("loses");
+        b.classList.add("loses");
       }
-    })
-
+    });
   }
   handleMouseOut(target) {
-    const arr = Array.from(target.closest('.choices').querySelectorAll('div'));
-    arr.map((item) => {
-      if (item.classList.contains('a')) {
+    const arr = Array.from(target.closest(".choices").querySelectorAll("div"));
+    arr.map(item => {
+      if (item.classList.contains("a")) {
         while (item.classList.length > 0) {
           item.classList.remove(item.classList.item(0));
         }
-        item.classList.add('a');
-      } else if (item.classList.contains('d')) {
+        item.classList.add("a");
+      } else if (item.classList.contains("d")) {
         while (item.classList.length > 0) {
           item.classList.remove(item.classList.item(0));
         }
-        item.classList.add('d');
-      } else if (item.classList.contains('b')) {
+        item.classList.add("d");
+      } else if (item.classList.contains("b")) {
         while (item.classList.length > 0) {
           item.classList.remove(item.classList.item(0));
         }
-        item.classList.add('b');
+        item.classList.add("b");
       }
-    })
+    });
   }
   handleKeyDown(key) {
-    const SysKeys = ['a', 'b', 'd', 'Enter'];
+    const SysKeys = ["a", "b", "d", "Enter"];
     if (SysKeys.indexOf(key) > -1) {
-      const ele = document.querySelector(`.${key}`)
-      if (key.toLowerCase() === 'a') {
+      const ele = document.querySelector(`.${key}`);
+      if (key.toLowerCase() === "a") {
         ele.focus();
-      } else if (key.toLowerCase() === 'b') {
+      } else if (key.toLowerCase() === "b") {
         ele.focus();
-      } else if (key.toLowerCase() === 'd') {
+      } else if (key.toLowerCase() === "d") {
         ele.focus();
-      } else if (key.toLowerCase() === 'enter') {
+      } else if (key.toLowerCase() === "enter") {
         ele.focus();
         this.handleSubmit();
         return;
       }
-      ele.querySelector('input').checked = true;
-      this.handleMouseOver(ele)
+      ele.querySelector("input").checked = true;
+      this.handleMouseOver(ele);
     }
   }
   handleClick(e) {
-    this.setState({ message: '' });
+    this.setState({ message: "" });
   }
   onMouseOver(e) {
     Services.debounce(this.handleMouseOver, 250)(e.target);
@@ -186,20 +195,22 @@ class Poll extends React.Component {
   }
 
   render() {
-
     if (this.state.fetchingErr) {
-      return <div>
-        <ShowMessage msg={this.state.message} onClick={this.handleClick} />
-        <div className="loading"> </div>
-      </div>;
+      return (
+        <div>
+          <ShowMessage msg={this.state.message} onClick={this.handleClick} />
+          <div className="loading"> </div>
+        </div>
+      );
     } else if (!this.state.data) {
       return <div className="loading"> </div>;
-
     }
 
-    const currentIsVoted = this.state.voted.find(obj => obj.vote.id === this.state.current.id)
-    const current = (currentIsVoted) ? currentIsVoted.vote : this.state.current;
-    const selectedOption = (currentIsVoted) ? currentIsVoted.selected : '';
+    const currentIsVoted = this.state.voted.find(
+      obj => obj.vote.id === this.state.current.id
+    );
+    const current = currentIsVoted ? currentIsVoted.vote : this.state.current;
+    const selectedOption = currentIsVoted ? currentIsVoted.selected : "";
     const teamA = current.homeName;
     const teamB = current.awayName;
     const sport = current.sport;
@@ -207,15 +218,24 @@ class Poll extends React.Component {
     const gameStatus = current.state;
     const group = current.group;
     const gameState = {
-      'NOT_STARTED': 'Not Started yet',
-      'STARTED': '🔥Already Started',
-      'FINISHED': 'Sorry!! you missed it'
+      NOT_STARTED: "Not Started yet",
+      STARTED: "🔥Already Started",
+      FINISHED: "Sorry!! you missed it"
     };
-    const randomMsg = ['Good Luck', 'finger crossed 🤞', 'Yes, Lest\'s go! 🏃', 'who cares..✌️', 'i will have it'];
-
+    const randomMsg = [
+      "Good s Luck",
+      "finger crossed 🤞",
+      "Yes, Lest's go! 🏃",
+      "who cares..✌️",
+      "i will have it"
+    ];
 
     return (
-      <section className={`main-container ${sport && sport.toLowerCase()}`} onKeyPress={this.onKeyPress} tabIndex="0">
+      <section
+        className={`main-container ${sport && sport.toLowerCase()}`}
+        onKeyPress={this.onKeyPress}
+        tabIndex="0"
+      >
         <div className="info">
           <div>
             <span className="label">Game Status </span>
@@ -223,72 +243,130 @@ class Poll extends React.Component {
           </div>
           <div>
             <span>Country</span>
-            <span>{flag}
-            </span>
-            <span className={`flag ${flag && flag.toLowerCase()}`}></span>
+            <span>{flag}</span>
+            <span className={`flag ${flag && flag.toLowerCase()}`} />
           </div>
           <div>
             <span>Group</span>
-            <span>
-              {group}
-            </span>
+            <span>{group}</span>
           </div>
           <div>
             <span>Game</span>
-            <span>
-              {sport}
-            </span>
+            <span>{sport}</span>
           </div>
         </div>
         <div className="choices" id="choices">
-          <div className="a" onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseOut} tabIndex="1">
-            <input type="radio" name="poll" value="1" id="a-win"
+          <div
+            className="a"
+            onMouseEnter={this.onMouseOver}
+            onMouseLeave={this.onMouseOut}
+            tabIndex="1"
+          >
+            <input
+              type="radio"
+              name="poll"
+              value="1"
+              id="a-win"
               defaultChecked={selectedOption === "1"}
-              onChange={this.handleChange} required />
+              onChange={this.handleChange}
+              required
+            />
             <label htmlFor="a-win">
-              <h2>{teamA} <span className="mod-icon"></span></h2>
+              <h2>
+                {teamA} <span className="mod-icon" />
+              </h2>
             </label>
             <span>home</span>
             <span className="icon">
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                <path d="M12.42 28.678l-12.433-12.238 6.168-6.071 6.265 6.167 13.426-13.214 6.168 6.071-19.594 19.285zM3.372 16.441l9.048 8.905 16.208-15.953-2.782-2.739-13.426 13.214-6.265-6.167-2.782 2.739z"></path>
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+              >
+                <path d="M12.42 28.678l-12.433-12.238 6.168-6.071 6.265 6.167 13.426-13.214 6.168 6.071-19.594 19.285zM3.372 16.441l9.048 8.905 16.208-15.953-2.782-2.739-13.426 13.214-6.265-6.167-2.782 2.739z" />
               </svg>
             </span>
           </div>
-          <div className="d" onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseOut} tabIndex="2">
-            <input type="radio" name="poll" value="2" id="draw"
+          <div
+            className="d"
+            onMouseEnter={this.onMouseOver}
+            onMouseLeave={this.onMouseOut}
+            tabIndex="2"
+          >
+            <input
+              type="radio"
+              name="poll"
+              value="2"
+              id="draw"
               defaultChecked={selectedOption === "2"}
-              onChange={this.handleChange} required />
+              onChange={this.handleChange}
+              required
+            />
             <label htmlFor="draw">
-              <h2>Draw <span className="mod-icon"></span></h2>
+              <h2>
+                Draw <span className="mod-icon" />
+              </h2>
             </label>
             <span className="icon">
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                <path d="M12.42 28.678l-12.433-12.238 6.168-6.071 6.265 6.167 13.426-13.214 6.168 6.071-19.594 19.285zM3.372 16.441l9.048 8.905 16.208-15.953-2.782-2.739-13.426 13.214-6.265-6.167-2.782 2.739z"></path>
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+              >
+                <path d="M12.42 28.678l-12.433-12.238 6.168-6.071 6.265 6.167 13.426-13.214 6.168 6.071-19.594 19.285zM3.372 16.441l9.048 8.905 16.208-15.953-2.782-2.739-13.426 13.214-6.265-6.167-2.782 2.739z" />
               </svg>
             </span>
           </div>
-          <div className="b" onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseOut} tabIndex="3">
-            <input type="radio" name="poll" value="3" id="b-win"
+          <div
+            className="b"
+            onMouseEnter={this.onMouseOver}
+            onMouseLeave={this.onMouseOut}
+            tabIndex="3"
+          >
+            <input
+              type="radio"
+              name="poll"
+              value="3"
+              id="b-win"
               defaultChecked={selectedOption === "3"}
-              onChange={this.handleChange} required />
+              onChange={this.handleChange}
+              required
+            />
             <label htmlFor="b-win">
-              <h2>{teamB} <span className="mod-icon"></span></h2>
+              <h2>
+                {teamB} <span className="mod-icon" />
+              </h2>
             </label>
             <span>away</span>
             <span className="icon">
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                <path d="M12.42 28.678l-12.433-12.238 6.168-6.071 6.265 6.167 13.426-13.214 6.168 6.071-19.594 19.285zM3.372 16.441l9.048 8.905 16.208-15.953-2.782-2.739-13.426 13.214-6.265-6.167-2.782 2.739z"></path>
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+              >
+                <path d="M12.42 28.678l-12.433-12.238 6.168-6.071 6.265 6.167 13.426-13.214 6.168 6.071-19.594 19.285zM3.372 16.441l9.048 8.905 16.208-15.953-2.782-2.739-13.426 13.214-6.265-6.167-2.782 2.739z" />
               </svg>
             </span>
           </div>
         </div>
-        <button className="btn-submit Enter" type="submit" onClick={this.handleSubmit} tabIndex="4">
-          Vote... <span className="random">{randomMsg[Services.random(0, 4)]}</span>
+        <button
+          className="btn-submit Enter"
+          type="submit"
+          onClick={this.handleSubmit}
+          tabIndex="4"
+        >
+          Vote...{" "}
+          <span className="random">{randomMsg[Services.random(0, 4)]}</span>
         </button>
         <ShowMessage msg={this.state.message} onClick={this.handleClick} />
       </section>
-    )
+    );
   }
 }
 
